8ca01598bfa52406e6391fc2fbb7a8a993b47e79
Diego Braga <diebraga0055@gmail.com>
2024-07-31 18:46:19 +0000

    handle login properly

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../environments/environment.prod';
 
 @Component({
   selector: 'app-login',
@@ -10,19 +12,41 @@ export class LoginComponent {
   email: string = '';
   password: string = '';
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private http: HttpClient
+  ) {}
 
   get isFormValid(): boolean {
     return !!this.email && !!this.password;
   }
 
   onSubmit() {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+
     const body = {
       email: this.email,
       password: this.password,
     };
 
-    console.log(body);
-    this.router.navigate(['/main']);
+    this.http
+      .post<{
+        token: string;
+      }>(`${environment.apiUrl}/api/signin`, body, { headers })
+      .subscribe({
+        next: (response) => {
+          localStorage.setItem('token', response.token);
+          this.router.navigate(['/main']);
+        },
+        error: (error) => {
+          if (error.status === 401) {
+            alert('Invalid credentials!');
+          } else {
+            console.error('There was an error!', error);
+          }
+        },
+      });
   }
 }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://glass-lewis-api-git-main-diebragas-projects.vercel.app',
+};
